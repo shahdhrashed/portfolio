5c1c3e997b4cc29f120c17f8c87c682e9d741bc6
AbdallahElraai <abdallahelrai1@gmail.com>
2026-06-17 14:40:51 +0300

    Favicon Update

---
+++ app/icon.tsx
@@ -0,0 +1,73 @@
+import { ImageResponse } from "next/og";
+import { readFileSync } from "fs";
+import { join } from "path";
+
+export const size = { width: 32, height: 32 };
+export const contentType = "image/png";
+
+export default async function Icon() {
+  const fontData = readFileSync(
+    join(process.cwd(), "app/fonts/modernline-bold.otf")
+  );
+
+  return new ImageResponse(
+    (
+      <div
+        style={{
+          width: "100%",
+          height: "100%",
+          display: "flex",
+          alignItems: "center",
+          justifyContent: "center",
+          background: "transparent",
+        }}
+      >
+        <div style={{ display: "flex", alignItems: "baseline" }}>
+          <span
+            style={{
+              fontFamily: "Modernline",
+              fontSize: 19,
+              fontWeight: 700,
+              color: "#c8154b",
+            }}
+          >
+            S
+          </span>
+          <span
+            style={{
+              fontFamily: "Modernline",
+              fontSize: 19,
+              fontWeight: 700,
+              color: "#c8154b",
+              marginLeft: "-0.42em",
+            }}
+          >
+            H
+          </span>
+          <span
+            style={{
+              fontFamily: "Modernline",
+              fontSize: 19,
+              fontWeight: 700,
+              color: "#c8154b",
+              marginLeft: "-0.15em",
+            }}
+          >
+            R.
+          </span>
+        </div>
+      </div>
+    ),
+    {
+      ...size,
+      fonts: [
+        {
+          name: "Modernline",
+          data: fontData,
+          style: "normal",
+          weight: 700,
+        },
+      ],
+    }
+  );
+}
