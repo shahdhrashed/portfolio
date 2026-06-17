cb8bd11eff02bfdc8bf09a1c4ebf66d75d544c9f
AbdallahElraai <abdallahelrai1@gmail.com>
2026-06-17 15:09:34 +0300

    favicon update #5

--- app/icon.tsx
+++ app/icon.tsx
@@ -2,7 +2,7 @@ import { ImageResponse } from "next/og";
 import { readFileSync } from "fs";
 import { join } from "path";
 
-export const size = { width: 32, height: 32 };
+export const size = { width: 64, height: 64 };
 export const contentType = "image/png";
 
 export default async function Icon() {
@@ -22,18 +22,43 @@ export default async function Icon() {
           background: "transparent",
         }}
       >
-        <span
-          style={{
-            fontFamily: "Modernline",
-            fontSize: 14,
-            fontWeight: 700,
-            color: "#c8154b",
-            letterSpacing: "-3px",
-            lineHeight: 1,
-          }}
-        >
-          SHR.
-        </span>
+        <div style={{ display: "flex", alignItems: "center" }}>
+          <span
+            style={{
+              fontFamily: "Modernline",
+              fontSize: 22,
+              fontWeight: 700,
+              color: "#c8154b",
+              lineHeight: 1,
+            }}
+          >
+            S
+          </span>
+          <span
+            style={{
+              fontFamily: "Modernline",
+              fontSize: 22,
+              fontWeight: 700,
+              color: "#c8154b",
+              marginLeft: "-0.42em",
+              lineHeight: 1,
+            }}
+          >
+            H
+          </span>
+          <span
+            style={{
+              fontFamily: "Modernline",
+              fontSize: 22,
+              fontWeight: 700,
+              color: "#c8154b",
+              marginLeft: "-0.15em",
+              lineHeight: 1,
+            }}
+          >
+            R.
+          </span>
+        </div>
       </div>
     ),
     {
